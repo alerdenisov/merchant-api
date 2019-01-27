60875efbb6640953d7456b5b4862f44598fa6c1a
Aler Denisov <aler.zampillo@gmail.com>
2019-01-27 07:52:55 +0300

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api/api.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+const packageJson: {
+  version: string;
+} = require('../package.json');
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Merchant API')
+    .setDescription('The Merchant API description')
+    .setVersion(packageJson.version)
+    .addTag('swagger')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

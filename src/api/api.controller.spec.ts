e94b454d33db76fa9b7345f8ffb9c9ff0990f312
Aler Denisov <aler.zampillo@gmail.com>
2019-01-27 07:45:19 +0300

    bootstrap module and service

---
+++ src/api/api.controller.spec.ts
@@ -0,0 +1,60 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ApiController } from './api.controller';
+
+describe('Api Controller', () => {
+  let controller: ApiController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ApiController],
+    }).compile();
+
+    controller = module.get<ApiController>(ApiController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('informational endpoints', () => {
+    describe('/basic_info', () => {
+      it('should response', async () => {});
+    });
+    describe('/rates', () => {
+      it('should response', async () => {});
+    });
+    describe('/balances', () => {
+      it('should response', async () => {});
+    });
+    describe('/deposit_address', () => {
+      it('should response', async () => {});
+    });
+    describe('/create_transaction', () => {
+      it('should response', async () => {});
+    });
+    describe('/callback_address', () => {
+      it('should response', async () => {});
+    });
+    describe('/tx_info', () => {
+      it('should response', async () => {});
+    });
+    describe('/txs_info', () => {
+      it('should response', async () => {});
+    });
+    describe('/txs', () => {
+      it('should response', async () => {});
+    });
+    describe('/create_transfer', () => {
+      it('should response', async () => {});
+    });
+    describe('/create_withdrawal', () => {
+      it('should response', async () => {});
+    });
+    describe('/withdrawal_history', () => {
+      it('should response', async () => {});
+    });
+    describe('/withdrawal_info', () => {
+      it('should response', async () => {});
+    });
+  });
+});

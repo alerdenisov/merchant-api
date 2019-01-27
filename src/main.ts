15c0fe35bda525c9b22e1a3f8330888468786a32
Aler Denisov <aler.zampillo@gmail.com>
2019-01-27 07:45:45 +0300

    simple server with API module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import { ApiModule } from './api/api.module';
 
 async function bootstrap() {
-  
-  // const app = await NestFactory.create(AppModule);
-  // await app.listen(3000);
+  const app = await NestFactory.create(ApiModule);
+  await app.listen(3000);
 }
 bootstrap();

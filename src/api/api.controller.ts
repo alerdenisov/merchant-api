e7e8e4edf9b9433d1fd470726de0a6fb41895bde
Aler Denisov <aler.zampillo@gmail.com>
2019-01-27 10:09:44 +0300

    getRates schema and responses

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -23,36 +23,88 @@ import {
   ApiForbiddenResponse,
   ApiImplicitHeader,
   ApiBadRequestResponse,
+  ResponseMetadata,
+  ApiImplicitHeaders,
 } from '@nestjs/swagger';
-import { GetBasicInfoResponse } from './dto/responses';
+import { GetBasicInfoResponse, GetRatesResponse } from './dto/responses';
 import { AuthenticationError, ValidationApiError } from './dto/errors';
 
 type Response<T> = Promise<T>;
+
+interface MethodOperationMeta {
+  title: string;
+  description?: string;
+  operationId?: string;
+  deprecated?: boolean;
+}
+
+type MethodResponseMeta = ResponseMetadata;
+
+interface MethodImplicitHeadersMeta {
+  name: string;
+  description?: string;
+  required?: boolean;
+}
+
+interface MethodSchema {
+  operation: MethodOperationMeta;
+  ok: MethodResponseMeta;
+  forbidden?: MethodResponseMeta;
+  badRequest?: MethodResponseMeta;
+  headers?: MethodImplicitHeadersMeta[];
+}
+
+type methods = keyof ApiController;
+
+const defaultForbidden = {
+  description: 'Forbinned',
+  type: AuthenticationError,
+};
+const defaultBadRequest = {
+  description: 'Call has been reject while processing',
+  type: ValidationApiError,
+};
+const defaultHeaders = [
+  {
+    name: 'HMAC',
+    description: 'Payload signature based on merchant secret phrase',
+  },
+];
+
+const apiSchema: { [method in methods]: MethodSchema } = {
+  getBasicInfo: {
+    operation: {
+      title: 'Get basic account information',
+      description:
+        'Returns object with identification string and private data setupped in admin panel',
+    },
+    ok: {
+      description: 'The bacis information has been successefully received',
+      type: GetBasicInfoResponse,
+    },
+  },
+  getRates: {
+    operation: {
+      title: 'Convertion Rates / Coin List',
+      description: 'Returns convertion rates and supported coins list',
+    },
+    ok: {
+      description: 'The list of coin has been successefully received',
+      type: GetRatesResponse,
+    },
+  },
+};
 
 @Controller('api')
 export class ApiController {
   constructor(private readonly apiService: ApiService) {}
 
   // Info
-
-  @ApiOperation({
-    title: 'Get basic account information',
-    description:
-      'Returns object with identification string and private data setupped in admin panel',
-  })
-  @ApiOkResponse({
-    description: 'The bacis information has been successefully received',
-    type: GetBasicInfoResponse,
-  })
-  @ApiForbiddenResponse({ description: 'Forbinned', type: AuthenticationError })
-  @ApiBadRequestResponse({
-    description: 'Call has been reject while processing',
-    type: ValidationApiError,
-  })
-  @ApiImplicitHeader({
-    name: 'HMAC',
-    description: 'Payload signature based on merchant secret phrase',
-  })
+  @ApiOperation(apiSchema.getBasicInfo.operation)
+  @ApiOkResponse(apiSchema.getBasicInfo.ok)
+  @ApiForbiddenResponse(apiSchema.getBasicInfo.forbidden || defaultForbidden)
+  @ApiBadRequestResponse(apiSchema.getBasicInfo.badRequest || defaultBadRequest)
+  @ApiImplicitHeaders(apiSchema.getBasicInfo.headers || defaultHeaders)
   @Post('/basic_info')
   async getBasicInfo(
     @Body() dto: GetBaseInfoRequest,
@@ -60,6 +112,11 @@ export class ApiController {
     return null;
   }
 
+  @ApiOperation(apiSchema.getRates.operation)
+  @ApiOkResponse(apiSchema.getRates.ok)
+  @ApiForbiddenResponse(apiSchema.getRates.forbidden || defaultForbidden)
+  @ApiBadRequestResponse(apiSchema.getRates.badRequest || defaultBadRequest)
+  @ApiImplicitHeaders(apiSchema.getRates.headers || defaultHeaders)
   @Post('/rates')
   async getRates(@Body() dto: GetRatesRequest): Response<any> {
     return null;
--- src/api/dto/responses.ts
+++ src/api/dto/responses.ts
@@ -26,3 +26,91 @@ export class GetBasicInfoResponse {
   })
   public_name: string;
 }
+
+export class CurrencyResponse {
+  @ApiModelProperty({
+    description: 'Currency short symbol (using as ID)',
+    required: true,
+  })
+  public symbol: string;
+
+  @ApiModelProperty({
+    description: 'Currency is fiat flag',
+    required: true,
+    default: false,
+  })
+  public is_fiat: boolean;
+
+  @ApiModelProperty({
+    description: 'Currency rate to BTC',
+    required: true,
+  })
+  public rate_btc: number;
+
+  @ApiModelProperty({
+    description: 'Currency rate to USD',
+    required: true,
+  })
+  public rate_usd: number;
+
+  @ApiModelProperty({
+    description: 'Currency rate to MNC',
+    required: true,
+  })
+  public rate_mnc: number;
+
+  @ApiModelProperty({
+    description: 'Timestamp of latest currency update',
+    required: true,
+  })
+  public last_update: number;
+
+  @ApiModelProperty({
+    description: 'Receive currency transaction fee',
+    required: true,
+  })
+  public receive_fee: number;
+
+  @ApiModelProperty({
+    description: 'Send currency transaction fee',
+    required: true,
+  })
+  public send_fee: number;
+
+  @ApiModelProperty({
+    description: 'Currency status in system',
+    enum: ['online', 'disabled', 'maintaince'],
+    default: 'online',
+    required: true,
+  })
+  public status: 'online' | 'disabled' | 'maintaince';
+
+  @ApiModelProperty({
+    description: 'Currency human-readable name',
+    required: true,
+  })
+  public name: string;
+
+  @ApiModelProperty({
+    description:
+      'Currency confirmation policy (Min number of confirmation to set as irreversible)',
+  })
+  public confirms: number;
+
+  @ApiModelProperty({
+    description: 'Currency available functionality',
+    enum: ['payments', 'wallet', 'transfers', 'convert'],
+    required: true,
+    isArray: true,
+  })
+  public capabilities: Set<'payments' | 'wallet' | 'transfers' | 'convert'>;
+}
+
+export class GetRatesResponse {
+  @ApiModelProperty({
+    isArray: true,
+    type: [CurrencyResponse],
+    required: true,
+  })
+  public list: Array<CurrencyResponse>;
+}
